505c70aacb4f7bdd029b87635467b30bbbf99f6f
Zhylow <pk1blue@yahoo.com>
2020-12-27 19:06:17 -0500

    created function 'generatePassword' and var 'userInputLength'

--- script.js
+++ script.js
@@ -18,16 +18,20 @@ function writePassword() {
 
 // ***********************funtion here*****************************************
 // write a funtion called generatepassword which will have a series of prompts
-
+function generatePassword() {
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
-
-
+    alert("Please choose the criteria for your password")
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
 // we have to ask the user for how many characters they want in the password
 // we have to check to make sure it is between 8 and 128
 // otherwise, we tell the user to fix their input
+    var userInputLength = prompt("Please choose the length of your password. (must be between 8 and 128 characters.)");
+        userInputLength = parseInt(userInputLength)
+          if (userInputLength < 8 || userInputLength > 128 || !Number.isInteger(userInputLength))
+            alert("Error! Please pick a number between 8 and 128");
+
 
 // WHEN prompted for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and/or special characters
@@ -44,7 +48,7 @@ function writePassword() {
 // THEN a password is generated that matches the selected criteria
 // from all the characters chosen, choose one randomly and add it to our password x number of times
 // 
-
+}
 // ***********************funtion ends here*****************************************
 
 // Add event listener to generate button
